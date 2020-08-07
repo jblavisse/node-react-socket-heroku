2a455e2b1ba7f5da53ba8bb7868025e87b45de82
Jlavisse <contact@jlavisse.com>
2020-08-07 12:00:47 +0200

    Update npm script for heroku

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
-
+const path = require('path');
 const port = 4000;
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client','build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
